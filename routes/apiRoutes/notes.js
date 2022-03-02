3a19f1b3baa8273f8f237ea2a8832b48710cb4a7
Raul <jimenezraul1981@gmail.com>
2022-03-01 23:54:57 -0500

    add creation of note to list

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,9 +1,23 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const notes = require('../../db/db.json')
+const { validateNote, createNewNote } = require("../../lib/notes");
 
-router.get('/notes', (req, res) => {
-  res.json(notes)
+const notes = require("../../db/db.json");
+
+router.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+router.post("/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send("The notes is not properly formatted.");
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
 });
 
 module.exports = router;
