4a74932ff0e92a6b669b9a99be1c5ab0e0847090
Raul <jimenezraul1981@gmail.com>
2022-03-02 19:24:25 -0500

    refactor apiroutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,28 @@
-const router = require('express').Router();
-const notesRoutes = require('./notes');
+const router = require("express").Router();
 
-router.use(notesRoutes);
+const { createNewNote, deleteNote } = require("../../lib/notes");
+
+let notes = require("../../db/db.json");
+
+// Get all notes
+router.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+// Post a new note
+router.post("/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+});
+
+// Delete a note by id
+router.delete("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const newArray = deleteNote(id, notes);
+  notes = newArray
+  res.json(newArray);
+});
 
 module.exports = router;
